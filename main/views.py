52653faaa441f339d328c61935e279eb0c691bb5
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 19:31:31 -0400

    Changed current page case to uppercase

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ import datetime
 
 def Index(request):
     data = {
-        'current_page': 'home',
+        'current_page': 'Home',
         'eboard': Eboard.objects.get(term='2017-2018'), #update term here to change eboard after creating new eboard db object
         'mission': ClubInfo.objects.get(pk=1).mission,
         'future_events': Event.objects.filter(end_date_time__gt=datetime.datetime.now()).order_by('start_date_time')[:2]
@@ -15,13 +15,13 @@ def Index(request):
 
 def Live(request):
     data = {
-        'current_page': 'live',
+        'current_page': 'Live',
     }
     return render(request,'main/live.html', data)
 
 def Events(request):
     data = {
-        'current_page': 'events',
+        'current_page': 'Events',
         'future_events': Event.objects.filter(end_date_time__gt=datetime.datetime.now()).order_by('start_date_time')
     }
     return render(request,'main/events.html', data)
