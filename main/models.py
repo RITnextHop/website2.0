ba9800f53b4fb9717a1c07455a2b636b58d40793
Brandt Woolf <bwoolf@cisco.com>
2017-08-11 21:06:12 -0400

    Initial model creations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Docs(models.Model):
+    title = models.CharField(max_length=100)
+    path = models.CharField(max_length=100)
+
+class Event(models.Model):
+    EVENT_TYPE = (
+        ('bin','Built-It-Night'),
+        ('tt','Tech Talk'),
+    )
+    type = models.CharField(choices=EVENT_TYPE)
+    title = models.CharField(max_length=500)
+    location models.CharField(max_length=100)
+    #get dow from date.weekday()
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    description = models.CharField(max_length=10000)
+    docs = models.ManyToManyField(Docs)
+    steam_url = models.URLField()
