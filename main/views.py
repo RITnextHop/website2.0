ed315b6b64a22132de6f54274b84b5c20b222a0b
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 17:00:37 -0400

    Added  query orderby

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ def Index(request):
         'current_page': 'home',
         'eboard': Eboard.objects.get(pk=1),
         'mission': ClubInfo.objects.get(pk=1).mission,
-        'future_events': Event.objects.filter(end_date_time__gt=datetime.datetime.now())[:2]
+        'future_events': Event.objects.filter(end_date_time__gt=datetime.datetime.now()).order_by('-start_date_time')[:2]
     }
     return render(request,'main/index.html', data)
 
