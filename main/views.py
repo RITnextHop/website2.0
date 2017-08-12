69c223348846c2bc4bba6b40807512cf090c0826
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 16:36:28 -0400

    Added future events to index

--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ def Index(request):
         'current_page': 'home',
         'eboard': Eboard.objects.get(pk=1),
         'mission': ClubInfo.objects.get(pk=1).mission,
+        'future_events': Event.objects.filter(end_date_time__gt=datetime.datetime.now())[:2]
     }
     return render(request,'main/index.html', data)
 
