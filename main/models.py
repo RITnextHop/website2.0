e7e9d0c9d2b24bb094c02cfa5c4b2637a85a73c4
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 20:12:11 -0400

    Slug blank allowed

--- main/models.py
+++ main/models.py
@@ -23,7 +23,7 @@ class Event(models.Model):
     description = models.TextField(max_length=10000)
     docs = models.ManyToManyField(Doc, blank=True)
     steam_url = models.URLField(blank=True)
-    url_slug = models.SlugField()
+    url_slug = models.SlugField(blank=True)
 
     def __str__(self):
         return self.title
