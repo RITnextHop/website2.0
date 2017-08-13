6b034faa35790d946996e41b287c332bb808bbff
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 22:53:14 -0400

    Adding helper text to stream link

--- main/models.py
+++ main/models.py
@@ -22,7 +22,7 @@ class Event(models.Model):
     end_date_time = models.DateTimeField()
     description = models.TextField(max_length=10000)
     docs = models.ManyToManyField(Resource, blank=True)
-    stream_url = models.URLField(blank=True)
+    stream_url = models.URLField(blank=True, help_text='If this is a YouTube link, ensure that /emded/ in include! Example: https://www.youtube.com/embed/EBYrGQoJ0Xg')
     #url_slug = models.SlugField(blank=True)
 
     @property
