2888b0ee523970a6950b622c7b6798849dee17b6
Brandt Woolf <bwoolf@cisco.com>
2017-08-12 22:44:32 -0400

    Changing doc to resource

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Doc)
+admin.site.register(Resource)
 admin.site.register(Event)
 admin.site.register(Eboard)
 admin.site.register(ClubInfo)
