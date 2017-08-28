280ad0bfd8ff6c66c324378de95a0ff18ce97c9c
Brandt Woolf <bwoolf@cisco.com>
2017-08-28 17:05:09 -0400

    creating doc page

--- main/views.py
+++ main/views.py
@@ -56,6 +56,8 @@ def EventPage(request, event_url):
         }
         return render(request, 'main/event_page.html', data)
 
+def Docs(request):
+    
 def DocPage(request, sem_folder, doc_name):
     print(sem_folder)
     print(doc_name)
