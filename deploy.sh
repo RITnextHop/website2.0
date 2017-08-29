c00ede22a62ecd104b98c9b0f78433b62f29141c
Brandt Woolf <bwoolf@cisco.com>
2017-08-28 21:04:42 -0400

    updated to v2 data

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git pull
 rm -rf db.sqlite3
 python3.6 manage.py migrate
 python3.6 manage.py migrate --run-syncdb
-python3.6 manage.py loaddata Initial\ Data/initial_data.yaml
+python3.6 manage.py loaddata Initial\ Data/initial_data_v2.yaml
