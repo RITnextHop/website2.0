e4764b0ac1a90ff1a0716c3f15ec07a6e08d9262
root <root@localhost.localdomain>
2017-10-18 09:16:57 -0400

    Updated to work with httpd

--- nexthop/wsgi.py
+++ nexthop/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www')
+sys.path.append('/var/www/nexthop')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nexthop.settings")
 
 application = get_wsgi_application()
